4fab667bc0a348db86c9a0ebcf36bfc424b3e224
robmachado <linux.rlm@gmail.com>
2017-08-18 11:20:35 -0300

    [UPD] add initial classes

---
+++ src/Common/Tools.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace NFePHP\EFDReinf\Common;
+
+use NFePHP\Common\Certificate;
+use NFePHP\eSocial\Common\XsdSeeker;
+use DateTime;
+
+class Tools
+{
+    /**
+     * @var string
+     */
+    protected $path;
+    /**
+     * @var DateTime
+     */
+    protected $date;
+    /**
+     * @var int
+     */
+    protected $tpInsc;
+    /**
+     * @var string
+     */
+    protected $nrInsc;
+    /**
+     * @var string
+     */
+    protected $nmRazao;
+    /**
+     * @var int
+     */
+    protected $tpAmb;
+    /**
+     * @var string
+     */
+    protected $verProc;
+    /**
+     * @var string
+     */
+    protected $eventoVersion;
+    /**
+     * @var string
+     */
+    protected $serviceVersion;
+    /**
+     * @var string
+     */
+    protected $layoutStr;
+    /**
+     * @var string
+     */
+    protected $serviceStr;
+    /**
+     * @var array
+     */
+    protected $serviceXsd = [];
+    /**
+     * @var Certificate
+     */
+    protected $certificate;
+    /**
+     * @var int
+     */
+    protected $transmissortpInsc;
+    /**
+     * @var string
+     */
+    protected $transmissornrInsc;
+    
+
+    /**
+     * Constructor
+     * @param string $config
+     * @param Certificate $certificate
+     */
+    public function __construct(
+        $config,
+        Certificate $certificate = null
+    ) {
+        //set properties from config
+        $stdConf = json_decode($config);
+        $this->tpAmb = $stdConf->tpAmb;
+        $this->verProc = $stdConf->verProc;
+        $this->eventoVersion = $stdConf->eventoVersion;
+        $this->serviceVersion = $stdConf->serviceVersion;
+        $this->date = new DateTime();
+        $this->tpInsc = $stdConf->empregador->tpInsc;
+        $this->nrInsc = $stdConf->empregador->nrInsc;
+        $this->nmRazao = $stdConf->empregador->nmRazao;
+        $this->transmissortpInsc = $stdConf->transmissor->tpInsc;
+        $this->transmissornrInsc = $stdConf->transmissor->nrInsc;
+        $this->certificate = $certificate;
+        
+        $this->path = realpath(
+            __DIR__ . '/../../'
+        ).'/';
+        
+        $this->serviceXsd = XsdSeeker::seek(
+            $this->path . "schemes/comunicacao/$this->serviceVersion/"
+        );
+    }
+}
---
+++ src/Factories/EvtAssocDespRec.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace NFePHP\EFDReinf\Factories;
+
+/**
+ * Class EFD-Reinf EvtAssocDespRec Event R-2030 constructor
+ *
+ * @category  API
+ * @package   NFePHP\EFDReinf
+ * @copyright NFePHP Copyright (c) 2017
+ * @license   http://www.gnu.org/licenses/lgpl.txt LGPLv3+
+ * @license   https://opensource.org/licenses/MIT MIT
+ * @license   http://www.gnu.org/licenses/gpl.txt GPLv3+
+ * @author    Roberto L. Machado <linux.rlm at gmail dot com>
+ * @link      http://github.com/nfephp-org/sped-efdreinf for the canonical source repository
+ */
+
+use NFePHP\EFDReinf\Common\Factory;
+use NFePHP\EFDReinf\Common\FactoryInterface;
+use NFePHP\EFDReinf\Common\FactoryId;
+use NFePHP\Common\Certificate;
+use stdClass;
+
+class EvtAssocDespRec extends Factory implements FactoryInterface
+{
+    /**
+     * @var string
+     */
+    protected $evtName = 'evtAssocDespRec';
+    /**
+     * @var string
+     */
+    protected $evtAlias = 'R-2030';
+
+    /**
+     * Constructor
+     * @param string $config
+     * @param stdClass $std
+     * @param Certificate $certificate
+     * @param string date
+     */
+    public function __construct(
+        $config,
+        stdClass $std,
+        Certificate $certificate = null,
+        $date = ''
+    ) {
+        parent::__construct($config, $std, $certificate, $date);
+    }
+    
+    /**
+     * Node constructor
+     */
+    protected function toNode()
+    {
+        
+    }
+    
+}
