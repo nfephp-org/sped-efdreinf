cae47d408cd8d84ba128a1b8b0f1948ceb43b96c
robmachado <linux.rlm@gmail.com>
2021-04-05 17:25:51 -0300

    inclusão versão 1.5.1

---
+++ examples/Fake/v1_05_01/Fake_R2020_EvtPrestadorServicos.php
@@ -0,0 +1,98 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+require_once '../../../bootstrap.php';
+
+use NFePHP\EFDReinf\Event;
+use NFePHP\Common\Certificate;
+use JsonSchema\Validator;
+
+$config = [
+    'tpAmb' => 2, //tipo de ambiente 1 - Produção; 2 - Produção restrita
+    'verProc' => '0_1_5_1', //Versão do processo de emissão do evento. Informar a versão do aplicativo emissor do evento.
+    'eventoVersion' => '1_05_01', //versão do layout do evento
+    'serviceVersion' => '1_05_01',//versão do webservice
+    'contribuinte' => [
+        'tpInsc' => 1,  //1-CNPJ, 2-CPF
+        'nrInsc' => '99999999', //numero do documento
+        'nmRazao' => 'Razao Social'
+    ],    
+    'transmissor' => [
+        'tpInsc' => 1,  //1-CNPJ, 2-CPF
+        'nrInsc' => '99999999999999' //numero do documento
+    ]
+];
+$configJson = json_encode($config, JSON_PRETTY_PRINT);
+
+$std = new \stdClass();
+$std->sequencial = 1;
+$std->indretif = 1;
+$std->nrrecibo = '1-00-1234-1234-1234556789012345';
+$std->perapur = '2017-11';
+$std->tpinscestabprest = 1;
+$std->nrinscestabprest = '12345678901234';
+$std->tpinsctomador = 1;
+$std->nrinsctomador = '12345678901234';
+$std->indobra = 1;
+$std->vlrtotalbruto = 1;
+$std->vlrtotalbaseret = 1;
+$std->vlrtotalretprinc = 1;
+$std->vlrtotalretadic = 1;
+$std->vlrtotalnretprinc = 1;
+$std->vlrtotalnretadic = 1;
+
+$std->nfs[0] = new \stdClass();
+$std->nfs[0]->serie = '001';
+$std->nfs[0]->numdocto = '265465';
+$std->nfs[0]->dtemissaonf = '2017-01-22';
+$std->nfs[0]->vlrbruto = 200.00;
+$std->nfs[0]->obs = 'observacao pode ser nula';
+
+$std->nfs[0]->infotpserv[0] = new \stdClass();
+$std->nfs[0]->infotpserv[0]->tpservico = '123456789';
+$std->nfs[0]->infotpserv[0]->vlrbaseret = 234.90;
+$std->nfs[0]->infotpserv[0]->vlrretencao = 12.75;
+$std->nfs[0]->infotpserv[0]->vlrretsub = 34.55;
+$std->nfs[0]->infotpserv[0]->vlrnretprinc = 2345.75;
+$std->nfs[0]->infotpserv[0]->vlrservicos15 = 22;
+$std->nfs[0]->infotpserv[0]->vlrservicos20 = 33;
+$std->nfs[0]->infotpserv[0]->vlrservicos25 = 44;
+$std->nfs[0]->infotpserv[0]->vlradicional = 5;
+$std->nfs[0]->infotpserv[0]->vlrnretadic = 1.55;
+
+$std->infoprocretpr[0] = new \stdClass();
+$std->infoprocretpr[0]->tpprocretprinc = 1;
+$std->infoprocretpr[0]->nrprocretprinc = 'ZYX987';
+$std->infoprocretpr[0]->codsuspprinc = '12345678901234';
+$std->infoprocretpr[0]->valorprinc = 200.98;
+
+$std->infoprocretad[0] = new \stdClass();
+$std->infoprocretad[0]->tpprocretadic = 1;
+$std->infoprocretad[0]->nrprocretadic = 'ACB21';
+$std->infoprocretad[0]->codsuspadic = '12345678901234';
+$std->infoprocretad[0]->valoradic = 1000.23;
+
+try {
+    
+   //carrega a classe responsavel por lidar com os certificados
+    $content     = file_get_contents('expired_certificate.pfx');
+    $password    = 'associacao';
+    $certificate = Certificate::readPfx($content, $password);
+    
+    //cria o evento e retorna o XML assinado
+    $xml = Event::evtServPrest(
+        $configJson,
+        $std,
+        $certificate,
+        '2017-08-03 10:37:00'
+    )->toXml();
+    
+    //$xml = Evento::r2020($json, $std, $certificate)->toXML();
+    //$json = Event::evtServPrest($configjson, $std, $certificate)->toJson();
+    
+    header('Content-type: text/xml; charset=UTF-8');
+    echo $xml;
+    
+} catch (\Exception $e) {
+    echo $e->getMessage();
+}
---
+++ src/Factories/EvtAqProd.php
@@ -0,0 +1,208 @@
+<?php
+
+namespace NFePHP\EFDReinf\Factories;
+
+/**
+ * Class EFD-Reinf EvtAqProd Event R-2055 constructor
+ *
+ * @category  API
+ * @package   NFePHP\EFDReinf
+ * @copyright NFePHP Copyright (c) 2017
+ * @license   http://www.gnu.org/licenses/lgpl.txt LGPLv3+
+ * @license   https://opensource.org/licenses/MIT MIT
+ * @license   http://www.gnu.org/licenses/gpl.txt GPLv3+
+ * @author    Roberto L. Machado <linux.rlm at gmail dot com>
+ * @link      http://github.com/nfephp-org/sped-efdreinf for the canonical source repository
+ */
+
+use NFePHP\EFDReinf\Common\Factory;
+use NFePHP\EFDReinf\Common\FactoryInterface;
+use NFePHP\EFDReinf\Common\FactoryId;
+use NFePHP\Common\Certificate;
+use stdClass;
+
+class EvtAqProd extends Factory implements FactoryInterface
+{
+    /**
+     * Constructor
+     * @param string $config
+     * @param stdClass $std
+     * @param Certificate $certificate
+     * @param string $data
+     */
+    public function __construct(
+        $config,
+        stdClass $std,
+        Certificate $certificate = null,
+        $data = ''
+    ) {
+        $params = new \stdClass();
+        $params->evtName = 'evt2055AquisicaoProdRural';
+        $params->evtTag = 'evtAqProd';
+        $params->evtAlias = 'R-2055';
+        parent::__construct($config, $std, $params, $certificate, $data);
+    }
+    
+    /**
+     * Node constructor
+     */
+    protected function toNode()
+    {
+        $ideContri = $this->node->getElementsByTagName('ideContri')->item(0);
+        //o idEvento pode variar de evento para evento
+        //então cada factory individualmente terá de construir o seu
+        $ideEvento = $this->dom->createElement("ideEvento");
+        $this->dom->addChild(
+            $ideEvento,
+            "indRetif",
+            $this->std->indretif,
+            true
+        );
+        $this->dom->addChild(
+            $ideEvento,
+            "nrRecibo",
+            !empty($this->std->nrrecibo) ? $this->std->nrrecibo : null,
+            true
+        );
+        $this->dom->addChild(
+            $ideEvento,
+            "perApur",
+            $this->std->perapur,
+            true
+        );
+        $this->dom->addChild(
+            $ideEvento,
+            "tpAmb",
+            $this->tpAmb,
+            true
+        );
+        $this->dom->addChild(
+            $ideEvento,
+            "procEmi",
+            $this->procEmi,
+            true
+        );
+        $this->dom->addChild(
+            $ideEvento,
+            "verProc",
+            $this->verProc,
+            true
+        );
+        $this->dom->addChild(
+            $ideEvento,
+            "retifS1250",
+            !empty($this->std->retifs1250) ? $this->std->retifs1250 : null,
+            false
+        );
+        $this->node->insertBefore($ideEvento, $ideContri);
+        
+        $info = $this->dom->createElement("infoAquisProd");
+        $ideEstab = $this->dom->createElement("ideEstabAdquir");
+        $this->dom->addChild(
+            $ideEstab,
+            "tpInscAdq",
+            $this->std->tpinscadq,
+            true
+        );
+        $this->dom->addChild(
+            $ideEstab,
+            "nrInscAdq",
+            $this->std->nrinscadq,
+            true
+        );
+        
+        $ideprod = $this->dom->createElement("ideProdutor");
+        $this->dom->addChild(
+            $ideprod,
+            "tpInscProd",
+            $this->std->tpinscprod,
+            true
+        );
+        $this->dom->addChild(
+            $ideprod,
+            "nrInscProd",
+            $this->std->nrinscprod,
+            true
+        );
+        $this->dom->addChild(
+            $ideprod,
+            "indOpcCP",
+            !empty($this->std->indopccp) ? $this->std->indopccp : null,
+            false
+        );
+        foreach($this->std->detaquis as $det) {
+            $detaq = $this->dom->createElement("detAquis");
+            $this->dom->addChild(
+                $detaq,
+                "indAquis",
+                $det->indaquis,
+                true
+            );
+            $this->dom->addChild(
+                $detaq,
+                "vlrBruto",
+                number_format($det->vlrbruto, 2, ',', ''),
+                true
+            );
+            $this->dom->addChild(
+                $detaq,
+                "vlrCPDescPR",
+                number_format($det->vlrcpdescpr, 2, ',', ''),
+                true
+            );
+            $this->dom->addChild(
+                $detaq,
+                "vlrRatDescPR",
+                number_format($det->vlrratdescpr, 2, ',', ''),
+                true
+            );
+            $this->dom->addChild(
+                $detaq,
+                "vlrSenarDesc",
+                number_format($det->vlrsenardesc, 2, ',', ''),
+                true
+            );
+            foreach($det->infoprocjud as $jud) {
+                $procjud = $this->dom->createElement("infoProcJud");
+                $this->dom->addChild(
+                    $procjud,
+                    "nrProcJud",
+                    $jud->nrprocjud,
+                    true
+                );
+                $this->dom->addChild(
+                    $procjud,
+                    "codSusp",
+                    !empty($jud->codsusp) ? $jud->codsusp : null,
+                    false
+                );
+                $this->dom->addChild(
+                    $procjud,
+                    "vlrCPNRet",
+                    !empty($jud->vlrcpnret) ? number_format($jud->vlrcpnret, 2, ',', '') : null,
+                    false
+                );
+                $this->dom->addChild(
+                    $procjud,
+                    "vlrRatNRet",
+                    !empty($jud->vlrratnret) ? number_format($jud->vlrratnret, 2, ',', '') : null,
+                    false
+                );
+                $this->dom->addChild(
+                    $procjud,
+                    "vlrSenarNRet",
+                    !empty($jud->vlrsenarnret) ? number_format($jud->vlrsenarnret, 2, ',', '') : null,
+                    false
+                );
+                $detaq->appendChild($procjud);
+            }
+            $ideprod->appendChild($detaq);
+        }
+        $ideEstab->appendChild($ideprod);
+        $info->appendChild($ideEstab);
+        $this->node->appendChild($info);
+        $this->reinf->appendChild($this->node);
+        //$this->xml = $this->dom->saveXML($this->reinf);
+        $this->sign($this->evtTag);
+    }
+}
